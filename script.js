a66e838aa958597d197765a174a1f3ac00b9f7aa
SaraToth <saratoth101@gmail.com>
2024-10-22 20:13:38 +0900

    Moved hashing function into the hashMap factory function and created the hash buckets with the default value being an empty array with a length of 16, and create basic set function that inputs the key and value and places them in a bucket

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function hash(key) {
-    let hashCode = 0;
-       
-    const primeNumber = 31;
-    for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+function hashMap() {
+    let buckets = new Array(16); // Array.apply(null, Array(16).map(function() {}));
+    return {
+        buckets: buckets,
+
+        hash: function(key) {
+            let hashCode = 0;
+            const primeNumber = 31;
+            for (let i = 0; i < key.length; i++) {
+                hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            }
+            
+            return hashCode % buckets.length;
+        },
+
+        set: function(key, value) {
+            const index = this.hash(key);
+
+            buckets[index] = value;
+        }
+
     }
- 
-    return hashCode;
-  } 
- 
+}
+
+const hashing = hashMap();
