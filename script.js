478797bf3aa349938de8901785adf37aba0fcfdb
SaraToth <saratoth101@gmail.com>
2024-10-22 14:56:41 +0900

    Add hash function, as provided by source project (ODIN Project), more info in README

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('tree');
+function hash(key) {
+    let hashCode = 0;
+       
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+ 
+    return hashCode;
+  } 
+ 
