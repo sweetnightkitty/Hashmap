f1359518dfda289580304befe9a5981f586f3b88
SaraToth <saratoth101@gmail.com>
2024-10-22 21:09:46 +0900

    Add modulus to hash function and create logic to input a key and get the associated value if it exists

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 function hashMap() {
-    let buckets = new Array(16); // Array.apply(null, Array(16).map(function() {}));
+    let defaultArray = new Array(16); // Array.apply(null, Array(16).map(function() {}));
     return {
-        buckets: buckets,
+        buckets: defaultArray,
 
         hash: function(key) {
             let hashCode = 0;
-            const primeNumber = 31;
+            const primeNumber = 49;
             for (let i = 0; i < key.length; i++) {
                 hashCode = primeNumber * hashCode + key.charCodeAt(i);
+                hashCode = hashCode % this.buckets.length;
             }
             
-            return hashCode % buckets.length;
+            return hashCode;
         },
 
         set: function(key, value) {
             const index = this.hash(key);
 
-            buckets[index] = value;
+            //how to compare this key, to the key at the index, if that index exists
+            this.buckets[index] = value;
+        },
+
+        get: function(key) {
+            const index = this.hash(key);
+
+            if(this.buckets[index]) {
+                return this.buckets[index];
+            } else {
+                return null;
+            }
         }
 
     }
